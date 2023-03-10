c478eac612f607dfb2a63ade0e9296a136803f7a
Dan Rousseau <danrousseau@protonmail.com>
2023-03-10 22:45:19 +0200

    chore: fix the example so it does not need to be run from multiple servers

--- examples/example.go
+++ examples/example.go
@@ -3,17 +3,10 @@ package main
 import (
 	"crypto/rand"
 	"encoding/base64"
-	"flag"
 	"time"
 
 	"lemur/messagequeue/client"
 	"lemur/messagequeue/server"
-)
-
-var (
-	isClient   bool
-	isConsumer bool
-	queueName  string
 )
 
 func randomString(length int) string {
@@ -26,28 +19,38 @@ func randomString(length int) string {
 }
 
 func main() {
-	flag.BoolVar(&isClient, "client", false, "is client")
-	flag.BoolVar(&isConsumer, "consumer", true, "consumer mode")
-	flag.StringVar(&queueName, "queue", "default", "queue name")
-	flag.Parse()
+	go func() {
+		runServer()
+	}()
 
-	if isClient {
-		if isConsumer {
-			client := client.NewSubscriber(":3000", queueName)
-			client.ReadFromQueue()
-		}
+	go func() {
+		time.Sleep(time.Second * 3)
+		runConsumer()
+	}()
 
-		if !isConsumer {
-			client := client.NewPublisher(":3000", queueName)
+	go func() {
+		time.Sleep(time.Second * 2)
+		runPublisher()
 
-			for {
-				time.Sleep(time.Millisecond * 20)
-				client.PublishMessage(randomString(10))
-			}
-		}
-	} else {
-		s := server.NewServer()
-		s.Start()
+	}()
+
+	select {}
+}
+
+func runServer() {
+	s := server.NewServer()
+	s.Start()
+}
+
+func runPublisher() {
+	publisher := client.NewPublisher(":3000", "test")
+	for {
+		time.Sleep(time.Millisecond * 20)
+		publisher.PublishMessage(randomString(10))
 	}
+}
 
+func runConsumer() {
+	consumer := client.NewSubscriber(":3000", "test")
+	consumer.ReadFromQueue()
 }
